83246e784f5b73811a40b74f9e175737d4ff6597
Dylan Park <dylan.park@att.net>
2023-10-11 15:18:32 -0500

    Core/basic plugin functionality

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Buffer } from "buffer";
 
 // Remember to rename these classes and interfaces!
 
@@ -10,58 +11,33 @@ const DEFAULT_SETTINGS: Obsidian64Settings = {
 	mySetting: 'default'
 }
 
+const decode = (str: string):string => Buffer.from(str, 'base64').toString('binary');
+
+const encode = (str: string):string => Buffer.from(str, 'binary').toString('base64');
+
 export default class Obsidan64 extends Plugin {
 	settings: Obsidian64Settings;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		
+		// This allows for encoding to Base64
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: 'encode-to-base64',
+			name: 'Encode to Base64',
+			editorCallback: (editor: Editor) => {
+				const selection = editor.getSelection();
+				editor.replaceSelection(encode(selection));
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+		
+		// This allows for decoding from Base64
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'decode-from-base64',
+			name: 'Decode From Base64',
+			editorCallback: (editor: Editor) => {
+				const selection = editor.getSelection();
+				editor.replaceSelection(decode(selection));
 			}
 		});
 
